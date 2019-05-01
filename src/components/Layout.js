adcace6f45e1388446b2cb12e1e863dfbba17f1b
MattHelton <mjoelhelton@me.com>
2019-05-01 10:35:46 -0400

    Refactors Layout class in Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,18 +1,18 @@
-import React, { memo } from 'react';
+import React from 'react';
 import { AppBar, Toolbar, Typography, Paper } from '@material-ui/core';
 
-const Layout = memo(props => (
-    <Paper
-        elevation={0}
-        style={{padding: 0, margin: 0, backgroundColor: "fafafa" }}
-    >
-        <AppBar color="primary" position="static" style={{ height: 64}}>
-            <Toolbar style={{ height: 64 }}>
-                <Typography color ='inherit'> TODO APP</Typography>
-            </Toolbar>
-        </AppBar>
-        {props.children}
-    </Paper>
-));
+class Layout extends React.Component {
+    render() {
+        return (
+            <Paper>
+                <AppBar>
+                    <Toolbar>
+                        <Typography></Typography>
+                    </Toolbar>
+                </AppBar>
+            </Paper>
+        )
+    }
+}
 
 export default Layout;
